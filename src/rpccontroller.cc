3ea99643faf0735af1577903de40e01d23c7029b
scott <scott@rpc.myguest.virtualbox.org>
2025-10-31 20:12:31 -0700

    controller

---
+++ src/rpccontroller.cc
@@ -0,0 +1,41 @@
+#include "./include/rpccontroller.h"
+
+rpccontroller::rpccontroller() {
+    m_failed = false;
+    m_errText = "";
+}
+
+void rpccontroller::Reset() {
+    m_failed = false; 
+    m_errText = ""; 
+}
+
+
+bool rpccontroller::Failed() const {
+    return m_failed; 
+}
+
+
+std::string rpccontroller::ErrorText() const {
+    return m_errText;
+}
+
+
+void rpccontroller::SetFailed(const std::string &reason) {
+    m_failed = true;   
+    m_errText = reason; 
+}
+
+
+void rpccontroller::StartCancel() {
+
+}
+
+
+bool rpccontroller::IsCanceled() const {
+    return false;  
+}
+
+void rpccontroller::NotifyOnCancel(google::protobuf::Closure* callback) {
+
+}
